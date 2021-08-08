8928980665c4cc72ddb2c829d30a9e87d070d2d3
Oleg Mazhanov <oleg.mazhanov@outlook.com>
2021-08-08 22:28:45 +1200

    Yahooo! Solved

--- Board/Task/src/board/GameBoardClass.kt
+++ Board/Task/src/board/GameBoardClass.kt
@@ -2,30 +2,27 @@ package board
 
 import java.security.Key
 
-class GameBoardClass<T>(width: Int) :GameBoard<T>, SquareBoardClass(width) {
+class GameBoardClass<T>(width1: Int) :GameBoard<T>, SquareBoardClass(width1) {
+    //val sqb:SquareBoardClass = SquareBoardClass(width1)
+    val cellsMap: MutableMap<Cell,T?> = mutableMapOf<Cell,T?>()
 
-    open val sqb:SquareBoardClass
-    open var cellsMap: MutableMap<Cell,T?> = mutableMapOf<Cell,T?>()
-
-
-    init {
-        sqb = SquareBoardClass(width)
-        for ( cell1 in this.sqb.cells ) {
-            this.cellsMap.put(cell1,null)
+    init{
+        for ( cell1 in cells ) {
+            cellsMap.put(cell1,null)
         }
     }
 
-    override fun get(cell: Cell): T? = this.cellsMap.get(cell)
+    override fun get(cell: Cell): T? = cellsMap.get(cell)
 
 
     override fun set(cell: Cell, v1: T?) {
-         this.cellsMap.put(cell,v1)
+         cellsMap.put(cell,v1)
     }
 
     override fun filter(predicate: (T?) -> Boolean): Collection<Cell> {
         //this.cellsMap.filter { it -> predicate(it.value) }.keys
         var cc: Collection<Cell> = mutableListOf()
-        for ((K,V) in this.cellsMap){
+        for ((K,V) in cellsMap){
             if (predicate(V)) {
                 cc += K
             }
@@ -35,7 +32,7 @@ class GameBoardClass<T>(width: Int) :GameBoard<T>, SquareBoardClass(width) {
 
     override fun find(predicate: (T?) -> Boolean): Cell? {
         //this.cellsMap.filter { it-> predicate(it.value)}.keys.single()
-        for ((K,V) in  this.cellsMap.entries){
+        for ((K,V) in  cellsMap.entries){
             if (predicate(V)) {
                 return K
             }
@@ -43,11 +40,11 @@ class GameBoardClass<T>(width: Int) :GameBoard<T>, SquareBoardClass(width) {
         return null
     }
 
-    override fun any(predicate: (T?) -> Boolean): Boolean = this.cellsMap
+    override fun any(predicate: (T?) -> Boolean): Boolean = cellsMap
             .filter { it-> predicate(it.value) }.asSequence().count().equals(0).not()
 
     override fun all(predicate: (T?) -> Boolean): Boolean {
-        var a1 = this.cellsMap
+        var a1 = cellsMap
             .filter{it-> !predicate(it.value) }.count()
         return a1 == 0
     }
